03a0b51432edf5baa3b4d14a99fc6b8ab9ce16d8
Pedro <pnc@cin.ufpe.br>
2021-04-30 23:39:06 -0300

    Atualização de coezao com o projeto

--- angular-http/src/app/models/car.ts
+++
@@ -1,6 +0,0 @@
-export interface Car {
-    id: number;
-    model: string;
-    color: string;
-    price: number;
-}
---
+++ angular-http/src/app/models/child.ts
@@ -0,0 +1,6 @@
+export interface Child {
+    name: string;
+    age: number;
+    severity: number;
+    responsible_pro: string;
+}
