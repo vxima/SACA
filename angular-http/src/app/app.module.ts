94f1d2616a71a9a8ad4a8d1ce373622513340904
Pedro <pnc@cin.ufpe.br>
2021-05-02 06:21:58 -0300

    Refatoração da pagina de registro e criação da barra de navegação

--- angular-http/src/app/app.module.ts
+++ angular-http/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MetasComponent } from './metas/metas.component';
-import { DesenvolvimentComponent } from './components/desenvolviment/desenvolviment.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterChildComponent } from './components/register-child/register-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MetasComponent,
-    DesenvolvimentComponent
+    NavBarComponent,
+    RegisterChildComponent
   ],
   imports: [
     BrowserModule,
