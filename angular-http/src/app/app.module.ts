2d0b63e04241ad67ff8c2ff7bb9ca18742ca32d9
Pedro <pnc@cin.ufpe.br>
2021-05-02 07:07:20 -0300

    Links para as funcionalidades já implementadas funcionando

--- angular-http/src/app/app.module.ts
+++ angular-http/src/app/app.module.ts
@@ -8,18 +8,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RegisterChildComponent } from './components/register-child/register-child.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    RegisterChildComponent
+    RegisterChildComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo:'home-page', pathMatch: 'full'
+      },
+      {
+        path: 'home-page', component:HomePageComponent
+      },
+      {
+        path: 'register-child', component:RegisterChildComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
