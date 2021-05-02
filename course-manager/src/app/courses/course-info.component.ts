d71825f690b545a507cab53025d19ff75c4f09eb
eneridangelis <emd@cin.ufpe.br>
2021-05-02 19:25:00 -0300

    course second module

---
+++ course-manager/src/app/courses/course-info.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
+import { CourseService } from './course.service';
+
+@Component({
+    templateUrl: './course-info.component.html'
+})
+export class CourseInfoComponent implements OnInit {
+
+    courseId!: number;
+
+    constructor(private activatedRoute: ActivatedRoute) { }
+    
+    ngOnInit(): void { 
+        this.courseId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+}
