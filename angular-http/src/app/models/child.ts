0031f22feeeebcddbaf60ef7b14e365130fd4814
Pedro <pnc@cin.ufpe.br>
2021-05-02 21:16:03 -0300

    Interfaces adicionadas

--- angular-http/src/app/models/child.ts
+++ angular-http/src/app/models/child.ts
@@ -1,7 +1,10 @@
+import { TaskGoals } from  './task-goals';
+
 export interface Child {
     id: number;
     name: string;
     age: number;
     severity: number;
     responsible_pro: string;
+    task_goals: TaskGoals[];
 }
---
+++ angular-http/src/app/models/task-goals.ts
@@ -0,0 +1,5 @@
+export interface TaskGoals {
+    task_id: number;
+    goal: string;
+    achieved: Boolean;
+}
