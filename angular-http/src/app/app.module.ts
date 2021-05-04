1b6cb7eacb48858122718690bf5d9583a5d7b8dd
kinhosz <jcsc@cin.ufpe.br>
2021-05-04 13:39:58 -0300

    criando nova funcionalidade

--- angular-http/src/app/app.module.ts
+++ angular-http/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { DependentTasksComponent } from './components/dependent-tasks/dependent-
 import { RegisterTaskComponent } from './components/register-task/register-task.component';
 import { ChildrenDetailingComponent } from './components/children-detailing/children-detailing.component';
 import { InformationsComponent } from './components/informations/informations.component';
+import { GoalStatusComponent } from './components/goal-status/goal-status.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { InformationsComponent } from './components/informations/informations.co
     DependentTasksComponent,
     RegisterTaskComponent,
     ChildrenDetailingComponent,
-    InformationsComponent
+    InformationsComponent,
+    GoalStatusComponent
   ],
   imports: [
     BrowserModule,
