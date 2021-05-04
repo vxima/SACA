cb945ea32816db4e77f48393f68bc2baee76eced
Pedro <pnc@cin.ufpe.br>
2021-05-04 14:27:34 -0300

    Teste da GUI feito

--- tests-acceptance/features/therefore_activities.feature
+++ tests-acceptance/features/therefore_activities.feature
@@ -1,23 +1,25 @@
 Feature: Como um profissional do SACA
-         Eu quero obter todas as atividades atribuidas a uma criança que precisam que dependem de uma atividade especifica para serem cumpridas
-         So that I can manage their learning goals
-         Para que eu possa identificar quantas atividades dependem de outra
+         Eu quero obter todas as atividades atribuidas a uma criança que dependem de uma atividade especifica para serem cumpridas
+         Para que eu possa identificar quantas e quais atividades dependem de outra
 
 ############################## GUI Scenarios  ##############################
 Scenario: Obter lista de quais atividades tem como pré-requisito "Desenhar ponto"
 Given Estou na pagina da criança com id "1"
 Given vejo o profissional responsavel "Jucelina" e o nome da criança "Ana"
+Given Seleciono o link "Atividades conseguintes"
 When Solicito as atividades que tem "Desenhar ponto" como pré-requisito
-Then Aparece uma lista com as atividades subsequentes ["Desenhar linha" , "Desenhar triangulo" , "Desenhar quadrado"]
+Then Aparece a lista dos Id's, titulos e descrições das atividades conseguintes, "2" "Desenhar linha" "A criança deverá desenhar um linha", "3" "Desenhar triangulo" "A criança deverá desenhar um triangulo", "4" "Desenhar quadrado" "A criança deverá desenhar um quadrado" em uma ordem qualquer
 
 Scenario: Obter lista de quais atividades tem como pré-requisito a atividade "Desenhar ponto" porém nenhuma atividade depende dela
 Given Estou na pagina da criança com id "2"
 Given vejo o profissional responsavel "Roberto" e o nome da criança "Jose"
+Given Seleciono o link "Atividades conseguintes"
 When Solicito as atividades que tem "Desenhar ponto" como pré-requisito
-Then Aparece uma lista com uma linha indicando que "Nenhuma atividade dependente"
+Then Aparece uma lista com uma linha verde indicando que "Nenhuma atividade dependente"
 
 Scenario: Obter lista de quais atividades tem como pré-requisito uma atividade não cadastrada
 Given Estou na pagina da criança com id "1"
 Given vejo o profissional responsavel "Jucelina" e o nome da criança "Ana"
+Given Seleciono o link "Atividades conseguintes"
 When Solicito as atividades que tem "Desenhar circulo" como pré-requisito
-Then Aparece uma lista com uma linha indicando que "Atividade não cadastrada"
+Then Aparece uma lista com uma linha laranja indicando que "Atividade não cadastrada"
--- tests-acceptance/stepdefinitions/therefores.ts
+++ tests-acceptance/stepdefinitions/therefores.ts
@@ -6,35 +6,131 @@ let expect = chai.expect;
 //let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
 //let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
-//let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+let cmp = ((elem, trio) => {
+    var sentence = true
+    sentence = sentence && elem.element(by.name('th_id')).getText().then(text => text === trio[0])
+    sentence = sentence && elem.element(by.name('th_title')).getText().then(text => text === trio[1])
+    sentence = sentence && elem.element(by.name('th_description')).getText().then(text => text === trio[2])
+    return sentence
+    
+});
 
-//let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+let cmpM = ((elem, val) => {
+    var sentence = true
+    sentence = sentence && elem.element(by.name('atividade_dependente')).getText().then(text => text === val)
+    return sentence
+    
+});
 
+
+let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+let code = 0
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Estou na pagina da criança com id "(\d*)"$/, async (id) => {
         await browser.get("http://localhost:4200/informations/"+id);
         await expect(browser.getTitle()).to.eventually.equal('SACA');
         //await $("a[name='alunos']").click();
     })
-/*
-    Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
-        var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
-        await allcpfs;
-        var samecpfs = allcpfs.filter(elem =>
-                                      elem.getText().then(text => text === cpf));
-        await samecpfs;
-        await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+    Given(/^vejo o profissional responsavel "([^\"]*)" e o nome da criança "([^\"]*)"$/, async (prof, child) => {
+
+        /*checar se o nome do surpervisor eh o mesmo */
+        var responsible_pro : ElementArrayFinder = element.all(by.name('td_child_responsible_pro'));
+        await responsible_pro;
+        var same_responsible_pro = responsible_pro.filter(elem =>
+                                      elem.getText().then(text => text === prof));
+        await same_responsible_pro;
+        await same_responsible_pro.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+        /*checar se o nome do da crianca eh o mesmo */
+
+        var name : ElementArrayFinder = element.all(by.name('td_child_name'));
+        await name;
+        var same_name = name.filter(elem =>
+                                      elem.getText().then(text => text === child));
+        await same_name;
+        await same_name.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+        /*clicar no botao que ativa a funcionalidade*/
+
     });
 
-    When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-        await $("input[name='namebox']").sendKeys(<string> name);
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await element(by.buttonText('Adicionar')).click();
+    Given(/^Seleciono o link "([^\"]*)"$/, async (therefore:string) => {
+        /* clicar no llink que leva para o componente da aplicacao */
+        await element(by.linkText(therefore)).click();
+        
     });
 
-    Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
-        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-        allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    When(/^Solicito as atividades que tem "([^\"]*)" como pré-requisito$/, async (atividade:string) => {
+        /* insere a atividade que vai ser testada */
+        await $("input[name='input_atividade']").sendKeys(<string> atividade);
     });
-    */
+
+    Then(/^Aparece a lista dos Id's, titulos e descrições das atividades conseguintes, "([^\"]*)" "([^\"]*)" "([^\"]*)", "([^\"]*)" "([^\"]*)" "([^\"]*)", "([^\"]*)" "([^\"]*)" "([^\"]*)" em uma ordem qualquer$/, 
+    async (id1:string, at1:string, ds1:string, id2:string, at2:string, ds2:string, id3:string, at3:string, ds3:string,) => 
+    {
+        /* Recebe o array de elemento que dentro tem as informacoes de cada atividade mostrada*/
+        var dependentes : ElementArrayFinder = element.all(by.name('atividade_list'));
+        await dependentes
+        /* Cria um array com os argumentos do cenario para comparar mais tarde */
+        var params = [[id1, at1, ds1], [id2, at2, ds2], [id3, at3, ds3]]
+        await params  
+
+        /* compara o tamanho das listas para ver se são compativeis para o loop*/
+        await dependentes.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(params.length));
+        /* ordena os arrays para evitar comparacoes erradas */
+        await (await dependentes).sort
+        await (await params).sort
+
+        /* para cada elemento compara seus valores com os do argumento */
+        for(let i = 0; i < params.length; i++) {
+            await dependentes.then(elems => expect(Promise.resolve(cmp((elems[i]),params[i]))).to.eventually.equal(true))
+        }
+
+        /* ver se mensagem de atividade se dependencia foi mostrada */
+        var mensage : ElementArrayFinder = element.all(by.name('atividade_dependente'));
+        await mensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+        /* ver se a mensagem de atividade nao cadastrada foi mostrada */
+        var wmensage : ElementArrayFinder = element.all(by.name('atividade_nc'));
+        await wmensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+
+    });
+    
+    Then(/^Aparece uma lista com uma linha verde indicando que "([^\"]*)"$/, 
+    async (arg:string) => 
+    {
+    
+        /* ver se mensagem de atividade se dependencia foi mostrada */
+        var mensage : ElementArrayFinder = element.all(by.name('atividade_dependente'));
+        await mensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+        /* ver se a mensagem de atividade nao cadastrada foi mostrada */
+        var wmensage : ElementArrayFinder = element.all(by.name('atividade_nc'));
+        await wmensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+        /* checa pra ver se alguma meta está sendo printada(o que nao pode acontecer)*/
+        var dependentes : ElementArrayFinder = element.all(by.name('atividade_list'));
+        await dependentes.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+        
+    });
+
+    Then(/^Aparece uma lista com uma linha laranja indicando que "([^\"]*)"$/, 
+    async (arg:string) => 
+    {
+    
+       /* ver se a mensagem de atividade nao cadastrada foi mostrada */
+       var wmensage : ElementArrayFinder = element.all(by.name('atividade_nc'));
+       await wmensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+       /* ver se mensagem de atividade se dependencia foi mostrada */
+       var mensage : ElementArrayFinder = element.all(by.name('atividade_dependente'));
+       await mensage.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+       /* checa pra ver se alguma meta está sendo printada(o que nao pode acontecer)*/
+       var dependentes : ElementArrayFinder = element.all(by.name('atividade_list'));
+       await dependentes.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+
 })
